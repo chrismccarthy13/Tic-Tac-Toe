ef341ebe7b17ea7497b8703b23b6ba0667bbee0b
EC2 Default User <ec2-user@ip-172-31-26-106.us-east-2.compute.internal>
2019-07-19 22:23:43 +0000

    initial commit

---
+++ src/Square.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import App from './App.js'
+import Board from './Board.js'
+
+function Square(props) {
+    return(
+        <button 
+        className="square" 
+        onClick ={props.onClick}>
+            {props.value}
+        </button>
+    );
+}
+    
+export default Square
